c4000b9d5559979bf2def83e60d9284d95d16faa
samiulmustakim <samiulmustakim99@gmail.com>
2026-03-07 17:14:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById('signinBtn').addEventListener('click', () => {
+    const userId = document.getElementById("user-id")
+    const userValue = userId.value
+    
+
+    const userpass = document.getElementById("user-pass")
+    const pin = userpass.value
+    
+
+    if (userValue === "admin" && pin === "admin123") {
+        window.location.assign("/home.html")
+    } else {
+        alert("Login Failed")
+        userId.value = ""
+        userpass.value = ""
+        return
+    }
+})
